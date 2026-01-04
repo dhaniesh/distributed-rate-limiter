c38bf68c561ffba843e50f3856b18719d9b4d893
Dhaniesh Murugan <53963269+dhaniesh@users.noreply.github.com>
2026-01-05 00:36:35 +0530

    add redis store

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,17 @@
-import { Store } from "./types";
+import { RateLimiterEntry, RedisStore } from "./types";
+import { redisClient } from "./services/redis";
 
-export const store: Store = {};
+export const store: RedisStore = {
+    async get(key: string): Promise<RateLimiterEntry | null> {
+        const value = await redisClient.get(key);
+        if (!value) return null
+        return JSON.parse(value);
+    },
+
+    async set(key: string, value: RateLimiterEntry): Promise<void> {
+        redisClient.set(key, JSON.stringify(value))
+    },
+    async del(key: string): Promise<void> {
+        await redisClient.del(key)
+    }
+}
