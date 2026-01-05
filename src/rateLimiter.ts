db6d6affdf35ca2fed21a0de8f6eb73f27f7441f
Dhaniesh Murugan <53963269+dhaniesh@users.noreply.github.com>
2026-01-05 23:39:50 +0530

    feat: add leaky bucket rate limiting support

--- src/rateLimiter.ts
+++ src/rateLimiter.ts
@@ -1,10 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-import { allowRequest } from "./services/fixedWindow/fixedWindow";
+import { allowFixedRequest } from "./services/fixedWindow/fixedWindow";
+import { allowLeakyRequest } from "./services/leakyBucket/leakyBucket";
 
 export function rateLimiter(limit: number, windowSize: number) {
     return async (req: Request, res: Response, next: NextFunction) => {
         const ip = req.ip || "unknown";
-        const requestallowed = await allowRequest(ip, limit, windowSize)
+        // const requestallowed = await allowFixedRequest(ip, limit, windowSize)
+        const requestallowed = await allowLeakyRequest(10)
         if (!requestallowed) {
             res.status(429).send("Too many requests");
             return;
