f225169927756d4166ef448c2ecfc5bc2d5ef039
Dhaniesh Murugan <53963269+dhaniesh@users.noreply.github.com>
2026-01-05 14:59:07 +0530

    feat(redis): add connection timeout and init function

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,4 +1,17 @@
 import { RedisClient } from "bun";
 
-export const redisClient = new RedisClient("redis://localhost:6379");
-await redisClient.connect();
+export const redisClient = new RedisClient("redis://localhost:6379", {
+    connectionTimeout: 1000
+});
+export async function initRedis() {
+    try {
+        console.log("Trying to connect to redis");
+
+        await redisClient.connect();
+        console.log("Connected to redis");
+    }
+    catch (error) {
+        console.log(`Failed to connect to redis, Error: ${error}`);
+        throw error
+    }
+}
