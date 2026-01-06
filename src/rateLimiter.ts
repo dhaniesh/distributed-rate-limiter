6e04654821dfb207e39aa652ec8fd6d0a85654a1
Dhaniesh Murugan <53963269+dhaniesh@users.noreply.github.com>
2026-01-06 23:37:33 +0530

    feat(rateLimiter): integrate token bucket rate limiting

--- src/rateLimiter.ts
+++ src/rateLimiter.ts
@@ -1,12 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 import { allowFixedRequest } from "./services/fixedWindow/fixedWindow";
 import { allowLeakyRequest } from "./services/leakyBucket/leakyBucket";
+import { allowTokenRequest } from "./services/tokenBucket/tokenBucket";
 
 export function rateLimiter(limit: number, windowSize: number) {
     return async (req: Request, res: Response, next: NextFunction) => {
         const ip = req.ip || "unknown";
         // const requestallowed = await allowFixedRequest(ip, limit, windowSize)
-        const requestallowed = await allowLeakyRequest(10)
+        // const requestallowed = await allowLeakyRequest(10)
+        const requestallowed = await allowTokenRequest()
         if (!requestallowed) {
             res.status(429).send("Too many requests");
             return;
